eac73cea9b4922683f3f184e06fa985631d1d01a
boneeyeamin <boneeyeamin21@gmail.com>
2023-06-12 21:59:29 +0600

    ager section thik hoisa

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".hero-slider").owlCarousel({
+        items:1,
+        loop:true,
+        smartSpeed: 900,
+        nav:true,
+        navText: [
+            '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg>',
+            '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>'
+        ]
+    });
+});
