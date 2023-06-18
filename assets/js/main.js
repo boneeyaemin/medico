065bbbb71f042c8826bb62b570ef7c13d21eebbe
boneeyeamin <boneeyeamin21@gmail.com>
2023-06-18 14:46:59 +0600

    issue fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
+
+
     $(".hero-slider").owlCarousel({
         items:1,
         loop:true,
         smartSpeed: 900,
         nav:true,
+        responsive:true,
+        responsive:{
+            0:{
+                nav:false,
+            },
+            576:{
+               nav:true,
+            },
+        },
         navText: [
             '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg>',
             '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>'
         ]
     });
-});
 
-$(document).ready(function(){
     $(".program-slider").owlCarousel({
         items:1,
         loop:true,
@@ -21,5 +30,18 @@ $(document).ready(function(){
             '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg>'
         ]
     });
-  });
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true
+    })
+
+
+
+
+
+});
+
+
+
 
